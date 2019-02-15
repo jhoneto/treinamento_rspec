d68f45924a01e21396401d60f0d8e1205ce85bab
Hamilton Oliveira <jhamilton.oliveira@gmail.com>
2019-02-15 14:10:48 -0300

    create first controller test

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -6,5 +6,26 @@ RSpec.describe StudentsController, type: :controller do
       get :index
       expect(response).to be_success
     end
+
+    it "returns a 200 response" do
+      get :index
+      expect(response).to have_http_status "200"
+    end    
+  end
+
+  describe "#new" do
+    it "add student with success" do
+      student_params = attributes_for(:student)
+      expect {
+        post :create, params: {student: student_params }
+      }.to change(Student, :count).by(1)
+    end
+
+    it "add student with unsuccess" do
+      student_params = attributes_for(:student, name: nil)
+      expect {
+        post :create, params: {student: student_params }
+      }.to_not change(Student, :count)
+    end    
   end
 end
